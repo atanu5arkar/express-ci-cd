0eb992c2f897f4117f5459247626d41033a879c1
atanu5arkar <atanu_sarkar1@outlook.com>
2024-12-17 08:17:44 +0530

    changes root route response

--- server/app.js
+++ server/app.js
@@ -4,9 +4,9 @@ const app = express();
 const port = process.env.PORT;
 
 app.get('/', (req, res) => {
-    return res.status(200).json({ msg: 'Welcome to CI/CD Route.' });
+    return res.status(200).json({ msg: `Welcome to CI/CD Route. Port=${port}` });
 })
 
 app.listen(port, () => {
     console.log('Server running at', port);
-})
+});
