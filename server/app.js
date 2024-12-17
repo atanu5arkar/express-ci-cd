2ef857b07fb3792ac20a4282a74939fd96f36960
atanu5arkar <atanu_sarkar1@outlook.com>
2024-12-17 07:59:46 +0530

    changes app.js

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.get('/', (req, res) => {
-    return res.status(200).json({ msg: 'Welcome to Home Route.' });
+    return res.status(200).json({ msg: 'Welcome to CI/CD Route.' });
 })
 
 app.listen(port, () => {
